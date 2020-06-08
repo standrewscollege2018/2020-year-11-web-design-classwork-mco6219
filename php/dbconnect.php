adc1de7c74b5f29f0ddbbdf44aa57c6e4253a393
mco6219 <mco6219@stacmail.net>
2020-06-09 09:52:53 +1200

    Create dbconnect.php

---
+++ php/dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+// the dbconnect.php file contains all the information that we need to connect to the database
+
+// we use the mysqli_connect()
+// this has 4 parameters: location, username, password, db name
+
+ ?>
