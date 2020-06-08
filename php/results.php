d730d2ed09a909c26347854d3d8ea0df521e9efb
mco6219 <mco6219@stacmail.net>
2020-06-09 10:21:46 +1200

    Update results.php

--- php/results.php
+++ php/results.php
@@ -1,9 +1,12 @@
 <?php
 // this page will run a query and display the result/s
 
+// we include the dbconnect.php code
+include("dbconnect.php");
+
 // there are 3 steps
 // 1. set up the query in a variable, this is what we want from the db
-$result_sql = "SELECT * from studentdb WHERE studentID=14"
+$result_sql = "SELECT * from student WHERE studentID=14";
 
 // 2. We go to the database and run the query
 // we use mysqli_query() this takes two parameters: dbconnect and query
@@ -11,7 +14,11 @@ $result_qry = mysqli_query($dbconnect, $result_sql);
 
 // 3. We organise our results into an associative array
 // which means we can use the column headings from the database table in our code
-// t organise the data from the db we use the mysql_fetch_assoc() function
-$result_aa = mysql_fetch_assoc($result_qry)
+// t organise the data from the db we use the mysqli_fetch_assoc() function
+// aa = associative array
+$result_aa = mysqli_fetch_assoc($result_qry);
 
+
+// display information from the db
+echo $result_aa['firstname'];
  ?>
