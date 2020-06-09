ad3c17a63b84a833fcc84e65f37a22935f5a8024
mco6219 <mco6219@stacmail.net>
2020-06-10 11:52:53 +1200

    Update results.php

--- php/results.php
+++ php/results.php
@@ -1,14 +1,14 @@
 <?php
 // this page will run a query and display the result/s
 
-// we include the dbconnect.php code
+// we include the dbconnect.php page, this connects this page with the db
 include("dbconnect.php");
 
 // there are 3 steps
 // 1. set up the query in a variable, this is what we want from the db
-$result_sql = "SELECT * from student WHERE studentID=14";
+$result_sql = "SELECT * from student WHERE studentID=2";
 
-// 2. We go to the database and run the query
+// 2. We go to the database and run the query, in this we are physically running the query 
 // we use mysqli_query() this takes two parameters: dbconnect and query
 $result_qry = mysqli_query($dbconnect, $result_sql);
 
