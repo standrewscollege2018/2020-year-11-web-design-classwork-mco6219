3c4d0fd6ce1ab1f88498568b51a07768a686eeb5
mco6219 <mco6219@stacmail.net>
2020-06-10 12:28:59 +1200

    search bars

--- php/results.php
+++ php/results.php
@@ -4,21 +4,31 @@
 // we include the dbconnect.php page, this connects this page with the db
 include("dbconnect.php");
 
+// grab the search content from the POST array, and put into a variable
+$search = $_POST['search'];
+
 // there are 3 steps
 // 1. set up the query in a variable, this is what we want from the db
-$result_sql = "SELECT * from student WHERE studentID=2";
+$result_sql = "SELECT * from student WHERE firstname LIKE '%$search%' OR lastname LIKE '%$search%'";
 
-// 2. We go to the database and run the query, in this we are physically running the query 
+// 2. We go to the database and run the query, in this we are physically running the query
 // we use mysqli_query() this takes two parameters: dbconnect and query
 $result_qry = mysqli_query($dbconnect, $result_sql);
 
 // 3. We organise our results into an associative array
 // which means we can use the column headings from the database table in our code
-// t organise the data from the db we use the mysqli_fetch_assoc() function
+// to organise the data from the db we use the mysqli_fetch_assoc() function
 // aa = associative array
 $result_aa = mysqli_fetch_assoc($result_qry);
 
+// define variables that you are pulling from the db
+$firstname = $result_aa['firstname'];
+$lastname = $result_aa['lastname'];
 
-// display information from the db
-echo $result_aa['firstname'];
  ?>
+ <!-- display data from the db -->
+ <div class="">
+  <p> <?php echo "$firstname $lastname";
+    ?></p>
+
+ </div>
---
+++ php/search.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- creating a form; search bar, log in page etc.-->
+      <!--make sure you enter required if the input is required and name the button appropriatly for your page -->
+    <form action="results.php" method="post">
+<input required type="text" name="search" placeholder="Enter your search">
+<button type="submit" name="button">Search</button>
+    </form>
+  </body>
+</html>
